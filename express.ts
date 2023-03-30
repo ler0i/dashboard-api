1121d447e4bf579eee433e7b8c80264e3aa15d9f
ler0i <63015777+ler0i@users.noreply.github.com>
2023-03-30 13:26:10 +0300

    ///

--- express.ts
+++ express.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction} from 'express';
 import { userRouter } from './users/users.js';
 
 const port = 8000;
@@ -44,7 +44,7 @@ app.get('/hello', (req, res) => {
 	
 });
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	console.log(err.message);
 	res.status(500).send(err.message);
 });
